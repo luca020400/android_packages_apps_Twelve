1e23a4373161f5948548300b79add666b2a4c56d
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-11-16 15:02:40 +0100

    Twelve: SubsonicDataSource: Remove not needed runCatching
    It can only happen if uri is null, it never is
    
    Change-Id: I8c2835ab42ed799c7e3d8e542c273c8b1e60f592

--- app/src/main/java/org/lineageos/twelve/datasources/SubsonicDataSource.kt
+++ app/src/main/java/org/lineageos/twelve/datasources/SubsonicDataSource.kt
@@ -305,23 +305,19 @@ class SubsonicDataSource(arguments: Bundle) : MediaDataSource {
         artistUri = artistId?.let { getArtistUri(it) } ?: Uri.EMPTY,
         artistName = artist ?: "",
         year = year,
-        thumbnail = runCatching {
-            Thumbnail(
-                uri = Uri.parse(subsonicClient.getCoverArt(id)),
-                type = Thumbnail.Type.FRONT_COVER,
-            )
-        }.getOrNull(),
+        thumbnail = Thumbnail(
+            uri = Uri.parse(subsonicClient.getCoverArt(id)),
+            type = Thumbnail.Type.FRONT_COVER,
+        )
     )
 
     private fun ArtistID3.toMediaItem() = Artist(
         uri = getArtistUri(id),
         name = name,
-        thumbnail = runCatching {
-            Thumbnail(
-                uri = Uri.parse(subsonicClient.getCoverArt(id)),
-                type = Thumbnail.Type.BAND_ARTIST_LOGO,
-            )
-        }.getOrNull(),
+        thumbnail = Thumbnail(
+            uri = Uri.parse(subsonicClient.getCoverArt(id)),
+            type = Thumbnail.Type.BAND_ARTIST_LOGO,
+        )
     )
 
     private fun Child.toMediaItem() = Audio(
