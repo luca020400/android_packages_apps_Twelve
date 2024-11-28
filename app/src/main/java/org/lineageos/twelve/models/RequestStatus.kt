4c4ef9263ccb5293f1621ebf7cc89350b24bc6bd
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-11-28 13:16:48 +0100

    Twelve: Allow passing a throwable to RequestStatus.Error
    Change-Id: I0671898ee88f4931919a52e97c8d408063886ce8

--- app/src/main/java/org/lineageos/twelve/models/RequestStatus.kt
+++ app/src/main/java/org/lineageos/twelve/models/RequestStatus.kt
@@ -30,5 +30,5 @@ sealed class RequestStatus<T, E> {
      *
      * @param error The error
      */
-    class Error<T, E>(val error: E) : RequestStatus<T, E>()
+    class Error<T, E>(val error: E, val throwable: Throwable? = null) : RequestStatus<T, E>()
 }
