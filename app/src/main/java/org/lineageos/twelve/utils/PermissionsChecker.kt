eff41e5d48613628f5a0ca9c2db4591c24796fe2
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-01-07 23:12:59 +0100

    Twelve: PermissionsChecker: Add optional flag and custom denied message
    Change-Id: I3fec7d457b726a349f535d2ba57231649df55c1a

--- app/src/main/java/org/lineageos/twelve/utils/PermissionsChecker.kt
+++ app/src/main/java/org/lineageos/twelve/utils/PermissionsChecker.kt
@@ -10,6 +10,7 @@ import android.content.Context
 import android.widget.Toast
 import androidx.activity.result.ActivityResultCaller
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
@@ -29,27 +30,37 @@ class PermissionsChecker private constructor(
     private val getActivity: () -> Activity,
     private val getLifecycle: () -> Lifecycle,
     private val permissions: Array<String>,
+    private val optional: Boolean = false,
+    @StringRes private val permissionsDeniedStringResId: Int? = R.string.app_permissions_toast,
 ) {
     constructor(
         fragment: Fragment,
         permissions: Array<String>,
+        optional: Boolean = false,
+        @StringRes permissionsDeniedStringResId: Int? = R.string.app_permissions_toast,
     ) : this(
         fragment,
         fragment::requireContext,
         fragment::requireActivity,
         fragment::lifecycle,
         permissions,
+        optional,
+        permissionsDeniedStringResId,
     )
 
     constructor(
         activity: AppCompatActivity,
         permissions: Array<String>,
+        optional: Boolean = false,
+        @StringRes permissionsDeniedStringResId: Int? = R.string.app_permissions_toast,
     ) : this(
         activity,
         { activity },
         { activity },
         activity::lifecycle,
         permissions,
+        optional,
+        permissionsDeniedStringResId,
     )
 
     private val channel = Channel<Unit>(1)
@@ -61,12 +72,16 @@ class PermissionsChecker private constructor(
 
         if (it.isNotEmpty()) {
             if (!context.permissionsGranted(permissions)) {
-                Toast.makeText(
-                    context,
-                    R.string.app_permissions_toast,
-                    Toast.LENGTH_SHORT
-                ).show()
-                getActivity().finish()
+                permissionsDeniedStringResId?.let { permissionsDeniedStringResId ->
+                    Toast.makeText(
+                        context,
+                        permissionsDeniedStringResId,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                if (!optional) {
+                    getActivity().finish()
+                }
             } else {
                 channel.trySend(Unit)
             }
